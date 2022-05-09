cdee41468d1a59d86fe8ef36f50facd21a471a3a
Ashraf Ali <ashrafalicitp@gmail.com>
2022-05-09 23:58:27 +0600

    mongodb connect to server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,7 +9,31 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+// Connect to Cluster
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdn4h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        await client.connect();
+        const serviceCollection = client.db('nevica').collection('service');
+
+        app.get('/service', async(req, res)=>{
+            const query= {};
+            const cursor= serviceCollection.find(query);
+            const services= await cursor.toArray();
+            res.send(services);
+            
+        })
+
+    }
+    finally{}
+}
+run().catch(console.dir);
+
+
+
+// root api
 app.get('/', (req, res)=>{
     res.send('Running Boating Nevica...');
 })
