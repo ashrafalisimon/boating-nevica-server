462cb6e72173ca9f00fe95bf6674078bf83a6fa8
Ashraf Ali <ashrafalicitp@gmail.com>
2022-05-14 14:05:15 +0600

    single service load

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -23,7 +23,13 @@ async function run(){
             const cursor= serviceCollection.find(query);
             const services= await cursor.toArray();
             res.send(services);
-            
+        })
+
+        app.get("/service/:id", async(req, res)=>{
+            const id =req.params.id;
+            const query={_id: ObjectId(id)};
+            const service= await serviceCollection.findOne(query);
+            res.send(service);
         })
 
     }
